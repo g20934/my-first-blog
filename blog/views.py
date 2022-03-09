1dc578b384332a4d81c404eac15cb3f2c4d9dae0
g20934 <g20934om@gm.tsuda.ac.jp>
2022-03-09 18:49:44 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,7 @@ def post_list(request):
   #公開したポストをpublised_dateで並び替えたクエリセットを取得する
   posts = Post.objects.filter(publised_date__lte=timezone.now()).order_by('publised_date')
   return render(request, 'blog/post_list.html', {'posts':posts})#前のpostsは名前、後ろのpostsは値（クエリセット）
+
+def post_detail(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  return render(request, 'blog/post_detail.html', {'post': post})
