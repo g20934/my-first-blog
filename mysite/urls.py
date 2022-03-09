8304432dc449387ca355d32a5931ca8612f29f21
g20934 <g20934om@gm.tsuda.ac.jp>
2022-03-09 13:56:32 +0900

    Changeed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #https://127.0.0.1:8000/
+    #に来たリクエストはblog.urlsへリダイレクトする
+    path('', include('blog.urls')),
 ]
